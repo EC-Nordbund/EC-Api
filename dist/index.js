1846c24593b325b6d5c396e9bbe5814ea6248023
Sebastian Krüger <2pi_r2@gmx.de>
2020-11-26 11:35:05 +0100

    remove dist, git

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("./express");
-const http = require("http");
-http.createServer(express_1.getApp(true)).listen(4000);
--- dist/version.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.appVersion = '1.2.1';
