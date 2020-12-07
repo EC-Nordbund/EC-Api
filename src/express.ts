892ea896a204477c665ae6c4a4c30c1c87a9149a
Sebastian Krüger <2pi_r2@gmx.de>
2020-12-07 16:26:54 +0100

    update-deps

--- src/express.ts
+++ src/express.ts
@@ -1,33 +1,22 @@
 import { schema } from './schema';
 import { appVersion } from './version';
-import { graphiqlExpress, graphqlExpress } from 'apollo-server-express';
-import * as bodyParser from 'body-parser';
+import { ApolloServer } from 'apollo-server-express';
+// import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
 import * as express from 'express';
 
 export const getApp = (dev: boolean) => {
+  const apollo = new ApolloServer({ schema })
   const app = express()
     .use(cors())
-    .use(
-      '/graphql',
-      bodyParser.json(),
-      graphqlExpress({
-        schema,
-      }),
-    )
     .use('/check', (req, res) => {
       res.end('{online: true}')
     })
-  if (dev) {
-    return app.use(
-      '/graphiql',
-      graphiqlExpress({
-        endpointURL: '/graphql',
-      }),
-    )
-  } else {
-    return app.use('/version', (req, res) => {
+    .use('/version', (req, res) => {
       res.end(`{"version": "${appVersion}"}`)
     })
-  }
+
+  apollo.applyMiddleware({ app, path: '/graphql' })
+
+  return app
 }
