4215f1f61ee5e3ebe1d0f53ec05bdec13d075493
Sebastian Krüger <2pi_r2@gmx.de>
2020-12-20 16:42:15 +0100

    add Personen liste

---
+++ src/api/personen.ts
@@ -0,0 +1,25 @@
+import { Express } from 'express'
+import { checkAuth } from '../auth'
+import { emptyObj } from '../types/types'
+import { errorHandler } from '../helpers/error'
+import { query } from '../helpers/mysql'
+import sql from 'sql-escape-tag'
+
+export default (app: Express): void => {
+  app.post<emptyObj, { personen: Array<any> }, emptyObj>(
+    '/v6/personen',
+    async (req, res) => {
+      try {
+        await checkAuth(req)
+        const personen = await query(
+          sql`SELECT personID, vorname, nachname, gebDat, geschlecht FROM personen`
+        )
+        res.json({
+          personen
+        })
+      } catch (error) {
+        errorHandler(error, res)
+      }
+    }
+  )
+}
