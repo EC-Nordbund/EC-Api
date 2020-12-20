e975a97006ad2aa6d8c6f40ca6488bb7acbc0b78
Sebastian Krüger <2pi_r2@gmx.de>
2020-12-20 16:43:40 +0100

    fix method

--- src/api/personen.ts
+++ src/api/personen.ts
@@ -6,7 +6,7 @@ import { query } from '../helpers/mysql'
 import sql from 'sql-escape-tag'
 
 export default (app: Express): void => {
-  app.post<emptyObj, { personen: Array<any> }, emptyObj>(
+  app.get<emptyObj, { personen: Array<any> }, emptyObj>(
     '/v6/personen',
     async (req, res) => {
       try {
