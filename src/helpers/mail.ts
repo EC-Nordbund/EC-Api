57ad459a88cc498c5ee8ed7b11e656136813da78
Sebastian Krüger <2pi_r2@gmx.de>
2020-12-21 10:38:02 +0100

    better mail + docs

--- src/helpers/mail.ts
+++ src/helpers/mail.ts
@@ -2,12 +2,39 @@ import { query } from './mysql'
 import { createTransport } from 'nodemailer'
 import { Readable } from 'stream'
 
+/**
+ * Empfänger Object
+ */
 interface empfaenger {
   to: string
   cc?: string
   bcc?: string
 }
 
+/**
+ * SMTP-Transport
+ */
+const smtp = createTransport({
+  host: process.env.SMTP_SERVER || '',
+  port: parseInt(process.env.SMTP_PORT || '1'),
+  auth: {
+    user: process.env.SMTP_USERNAME || '',
+    pass: process.env.SMTP_PASSWORD || ''
+  }
+})
+
+/**
+ * Sendet eine E-Mail mit den gegeben Parametern
+ *
+ * @author Sebastian
+ *
+ * @param from Absender E-Mail (muss mit @ec-nordbund.de enden)
+ * @param e Empfänger (TO, CC, BCC)
+ * @param subject Betreff
+ * @param body Body
+ * @param isHTML Ist Body HTML
+ * @param attachments Array von Attachments
+ */
 export default async function sendMail(
   from: string,
   e: empfaenger,
@@ -19,19 +46,11 @@ export default async function sendMail(
     filename: string
   }> = []
 ): Promise<true> {
-  const smtp = createTransport({
-    host: process.env.SMTP_SERVER || '',
-    port: parseInt(process.env.SMTP_PORT || '1'),
-    auth: {
-      user: process.env.SMTP_USERNAME || '',
-      pass: process.env.SMTP_PASSWORD || ''
-    }
-  })
   const mailData = {
     from,
     to: e.to,
-    cc: e.cc || '',
-    bcc: e.bcc || '',
+    cc: e.cc ?? '',
+    bcc: e.bcc ?? '',
     subject,
     ...(isHTML ? { html: body } : { text: body }),
     attachments: attachments.map((at) => ({
